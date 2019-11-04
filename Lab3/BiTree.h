05a9ddf09528673aa1aa9a840b972f6e69172791
john Maxwell <iuiuiepi@outlook.com>
2019-11-04 23:52:12 +0800

    Lab3 h

--- Lab3/BiTree.h
+++ Lab3/BiTree.h
@@ -5,4 +5,46 @@
 #ifndef LAB3_BITREE_H
 #define LAB3_BITREE_H
 
+#include <memory>
+
+template<typename T>
+class TreeHead;
+
+template<typename T>
+class TreeNode {
+private:
+    T _val;
+
+    std::unique_ptr<TreeNode> _leftNode;
+
+    std::unique_ptr<TreeNode> *_rightNode;
+
+    TreeNode();
+
+    TreeNode(T val);
+
+public:
+    [[nodiscard]]inline const T &val() const { return _val; }
+
+    friend class TreeHead<T>;
+};
+
+template<typename T>
+class TreeHead {
+private:
+    std::unique_ptr<TreeNode<T>> _root;
+public:
+    TreeHead();
+
+    TreeHead(std::string, std::string);
+
+    void clear();
+
+    bool empty();
+
+    int depth();
+
+    const T &locate(T);
+};
+
 #endif //LAB3_BITREE_H
